38a216883e50c09162b0c290b1d2efb517693555
Brian Ingles <github@emeraldwalk.com>
2025-03-31 23:14:01 -0500

    Godot client

---
+++ client/plugin.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+	add_custom_type("SignalLobby", "Node", preload("lobby_node.gd"), preload("icon.png"))
+
+
+func _exit_tree() -> void:
+	remove_custom_type("SignalLobby")
